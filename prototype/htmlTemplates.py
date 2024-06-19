6bc88c36fc79dd366768db74281362e5e5aa3351
elisehammarstrom <32701757+elisehammarstrom@users.noreply.github.com>
2024-06-19 10:26:39 +0200

    commented out the images in html templates

--- prototype/htmlTemplates.py
+++ prototype/htmlTemplates.py
@@ -27,18 +27,22 @@ css = '''
 
 bot_template = '''
 <div class="chat-message bot">
-    <div class="avatar">
-        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
-    </div>
+    <!--  
+        <div class="avatar">
+            <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        </div>
+    -->
     <div class="message">{{MSG}}</div>
 </div>
 '''
 
 user_template = '''
 <div class="chat-message user">
-    <div class="avatar">
-        <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
-    </div>    
+    <!--  
+        <div class="avatar">
+                <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
+        </div>   
+    --> 
     <div class="message">{{MSG}}</div>
 </div>
 '''
