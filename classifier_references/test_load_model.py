b7c4a7f3304353f44c34df456f169355ea617516
vaughankraska <vaughankraska@gmail.com>
2024-07-05 14:01:42 +0200

    added one lil test thing to markuses code for loading the classifier model.

--- classifier_references/test_load_model.py
+++ classifier_references/test_load_model.py
@@ -5,7 +5,9 @@ output_dir = "./textcat_model"
 nlp = spacy.load(output_dir)
 
 # Use the loaded model to make predictions
-texts = ["This is a test text to classify", "Another example text"]
+texts = ["This is a test text to classify", 
+         "Another example text",
+         "Anglemyer A, Horvath T, Rutherford G (2014). The accessibility of ﬁrearms and risk for suicide and homicide victimization among household members: a systematic review and meta-analysis. Annals of Internal Medicine 160, 101–110. doi: 10.7326/M13-1301."]
 for text in texts:
     doc = nlp(text)
     print(text, doc.cats)
